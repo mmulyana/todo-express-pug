94649645cf693f1d0f0e3dc76eb3f99785cd159b
mmulyana <mulyan.t20@gmail.com>
2024-01-01 13:00:00 +0700

    feat: setup express, pug

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const todos = require('./data/todos')
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(8000, () => {
+  console.log(`server is running http://localhost:8000`)
+})
